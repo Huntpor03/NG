0e145cac7d0ade467aa42aea0fe7e4be26d8e867
Huntpor03 <Huntpor03@gmail.com>
2024-06-10 23:41:28 -0700

    added Red/ Orange
    Added Red and Orange, added Yellow to start that as well.

--- src/main/java/sbtech/NG/ColorLN.js
+++ src/main/java/sbtech/NG/ColorLN.js
@@ -1,9 +1,50 @@
+//CONTAINERS
 const tileContainer = document.querySelector('.tiles');
+const itemContainer = document.querySelector(".fitems");
+
+
+// COLOR ARRAY
 const colors = ["Red","Orange","Yellow","Green","Blue","Purple","White","Black","Brown","Pink"];
+
+
+//EMPTY IMAGES
+const reditems = ["emptyapple","emptyheart","emptycrayon","emptystrawberry","emptyraspberry"];
+const orangeitems = ["emptycarrot","emptypumpkin","emptyorange","emptybasketball","emptyfire"];
+
+const yellowitems = ["bananna","sunflower","sponge","corn","duck"];
+const greenitems = ["frog","clover","grass","pear","trees"];
+const blueitems = ["blueberry","whale","jeans","balloon","car"];
+const purpleitems = ["grapes","paint","jellyfish","crystal","shoes"];
+
+//FILLED IMAGES
+const reditemsfilled = ["apple","heart","crayon","strawberry","raspberry"];
+const orangeitemsfilled = ["carrot","pumpkin","orange","basketball","fire"];
+const yellowitemsfilled = [""];
+
+// COLOR / IMAGE PICKERS
 const colorpick = [...colors];
+const redpicker = [...reditems];
+const orangepicker = [...orangeitems];
+const yellowpicker = [...yellowitems];
+const greenpicker = [...greenitems];
+const bluepicker = [...blueitems];
+const purplepicker = [...purpleitems];
+
+// FILLED IMAGE PICKERS
+const redfilleditem = [...reditemsfilled];
+const orangefilleditem = [...orangeitemsfilled];
+const yellowfilleditem = [...yellowitemsfilled];
+
+//COUNTERS
 const tileCount = colorpick.length;
+const redcount = redpicker.length;
+const orangecount = orangepicker.length;
+const yellowcount = yellowpicker.length;
+
+
 
 const clicked = document.getElementById("clicked");
+
 
 function BuildTile(color){
 	const newTile = document.createElement("div");
@@ -24,7 +65,7 @@ function BuildTile(color){
 		clicked.innerText = color;
 		
 		
-		if(clicked.innerText = color){
+		if(clicked.innerText == "Red"){
 			function reditem(){
 	
 				const reditem = document.createElement("img");
@@ -35,26 +76,73 @@ function BuildTile(color){
 	
 					reditem.addEventListener("click", () => {
 		
-						reditem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + filleditem.shift() + ".jpg";
+						reditem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + redfilleditem.shift() + ".jpg";
 	
 				});
 	
 					return reditem;
 			}
-
+	
 
 
 			for (let r = 0; r < redcount; r++){
 	
-				//const randomitems = Math.floor(Math.random() * redpicker.length);
 				const colored = redpicker[reditems.length];
 				const itemTile = reditem(colored);
-				
-				//redpicker.splice(randomitems, 1);
 				itemContainer.appendChild(itemTile);
 			}
 		
-		}
+		 }
+		 
+		 if (clicked.innerText == "Orange"){
+			function orangeitem(){
+				const orangeitem = document.createElement("img");
+				
+				orangeitem.classList.add("item");
+				
+				orangeitem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + orangepicker.shift() + ".jpg";
+				
+				orangeitem.addEventListener("click", () => {
+		
+						orangeitem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + orangefilleditem.shift() + ".jpg";
+	
+				});	
+				
+				return orangeitem;
+		 	}
+		 	
+		 	for(let o = 0; o < orangecount; o++){
+				const orangecolored = orangepicker[orangeitems.length];
+				const itemTile = orangeitem(orangecolored);
+				
+				itemContainer.appendChild(itemTile);
+				
+			}
+		 	
+		 }
+		 
+		 if(clicked.innerText == "Yellow"){
+			function yellowitem(){
+				const yellowitem = document.createElement("img");
+				
+				yellowitem.classList.add("item");
+				
+				yellowitem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + yellowpicker.shift() + ".jpg";
+				
+				yellowitem.addEventListener("click", () => {
+					
+					yellowitem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + yellowfilleditem.shift() + ".jpg";
+				});
+				
+				return yellowitem;
+			}
+		 }
+		 for(let y = 0; y < yellowcount; y++){
+			const yellowcolored = yellowpicker[yellowitems.length];
+			const yellowTile = yellowitem(yellowcolored);
+			itemContainer.appendChild(yellowTile);
+		 }
+		 
 	});
 	
 	return newTile;
@@ -69,83 +157,3 @@ for (let i = 0; i < tileCount; i++){
 	colorpick.splice(randomIndex, 1);
 	tileContainer.appendChild(tile);
 }
-
-const itemContainer = document.querySelector(".fitems");
-
-const reditems = ["apple","emptyheart","crayon","emptystrawberry","rasbarry"];
-const reditemsfilled = ["heart","strawberry"];
-const orangeitems = ["carrot","pumpkin","orange","basketball","fire"];
-const yellowitems = ["bananna","sunflower","sponge","corn","duck"];
-const greenitems = ["frog","clover","grass","pear","trees"];
-const blueitems = ["blueberry","whale","jeans","balloon","car"];
-const purpleitems = ["grapes","paint","jellyfish","crystal","shoes"];
-
-const itempicker = [...reditems,...blueitems,...orangeitems,...yellowitems,...greenitems,...purpleitems];
-const itemCount = itempicker.length;
-
-const redpicker = [...reditems];
-
-const redcount = redpicker.length;
-
-const filleditem = [...reditemsfilled];
-
-/*function BuildItem(){
-	
-	const newItem = document.createElement("img");
-	
-	newItem.classList.add("item");
-	
-	newItem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + itempicker.shift() + ".jpg";
-	
-	newItem.addEventListener("click", () => {
-		
-			newItem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + filleditem.shift() + ".jpg";
-	
-	});
-	
-	return newItem;
-}
-
-*/
-/*
-function reditem(){
-	
-	const reditem = document.createElement("img");
-	
-	reditem.classList.add("item");
-	
-	reditem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + redpicker.shift() + ".jpg";
-	
-	reditem.addEventListener("click", () => {
-		
-			reditem.src = "../wicket/resource/sbtech.NG.ColorLN/colorimg/" + filleditem.shift() + ".jpg";
-	
-	});
-	
-	return reditem;
-}
-
-
-
-for (let r = 0; r < redcount; r++){
-	
-	//const randomitems = Math.floor(Math.random() * redpicker.length);
-	const colored = redpicker[reditems.length];
-	const itemTile = reditem(colored);
-	
-	//redpicker.splice(randomitems, 1);
-	itemContainer.appendChild(itemTile);
-}
-
-
-/*
-for (let j = 0; j < itemCount; j++){
-	
-	const randomitems = Math.floor(Math.random() * itempicker.length);
-	const colored = itempicker[randomitems];
-	const itemTile = BildItem(colored);
-	
-	itempicker.splice(randomitems, 1);
-	itemContainer.appendChild(itemTile);
-}
-*/
