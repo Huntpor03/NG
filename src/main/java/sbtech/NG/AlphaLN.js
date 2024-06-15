77528cb91a6a3ee1dbd4e206634490e6aa8f4680
Huntpor03 <Huntpor03@gmail.com>
2024-06-15 16:43:44 -0700

    Changes

--- src/main/java/sbtech/NG/AlphaLN.js
+++ src/main/java/sbtech/NG/AlphaLN.js
@@ -1,42 +1,72 @@
 const alphabetContainer = document.querySelector(".alphabet");
 const choiceContainer = document.querySelector(".choiceletter");
-const aContainer = document.querySelector(".aletter");
-const bContainer = document.querySelector(".bletter");
-const fill = document.querySelector('.fill');
+
+const fill = document.querySelectorAll('.fill');
 const empties = document.querySelectorAll('.empty');
 const alphabet = ["Aa","Bb","Cc", "Dd","Ee","Ff","Gg","Hh", "Ii","Jj","Kk","Ll","Mm","Nn","Oo","Pp","Qq","Rr","Ss","Tt","Uu","Vv","Ww","Xx","Yy","Zz"];
-const aimg = ['A'];
-const bimg = ['B'];
+const aimg = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 const alphabetpicker = [...alphabet];
+
+const letterpicker = [...aimg];
 const alphabetCount = alphabetpicker.length;
 
 const letterTitle = document.getElementById("lettered");
 
-aContainer.addEventListener('dragstart', dragStart);
-aContainer.addEventListener('dragend', dragEnda);
-bContainer.addEventListener('dragstart', dragStart);
-bContainer.addEventListener('dragend', dragEndb);
+const img = document.createElement('img');
 
-aimg.forEach(function(image){
-	const img = document.createElement('img');
-	img.src = "../wicket/resource/sbtech.NG.AlphaLN/alphaimg/" + image + ".jpg";
-	img.classList.add('fill');
-	img.id = image;
-	choiceContainer.appendChild(aContainer);
-	aContainer.append(img);
-	
-	return image;
-});
-bimg.forEach(function(image){
-	const img = document.createElement('img');
-	img.src = "../wicket/resource/sbtech.NG.AlphaLN/alphaimg/" + image + ".jpg";
-	img.classList.add('fill');
-	img.id = image;
-	choiceContainer.appendChild(bContainer);
-	bContainer.append(img);
-	
-	return image;
-});
+let draggedItem = null;
+
+//Container.addEventListener('dragstart', dragStart);
+//aContainer.addEventListener('dragend', dragEnda);
+//bContainer.addEventListener('dragstart', dragStart);
+//bContainer.addEventListener('dragend', dragEndb);
+
+
+for(const empty of empties){
+	empty.addEventListener('dragover', function dragOver(e) {
+			console.log('dragover');
+			e.preventDefault();
+		});
+	empty.addEventListener('dragenter', function dragEnter(e) {
+			console.log('dragenter');
+			e.preventDefault();
+			this.className += ' hovered';
+		});
+	empty.addEventListener('dragleave', function dragLeave() {
+			console.log('dragleave');
+			this.className = 'empty';
+		});
+		empty.addEventListener('drop', function dragDrop() {
+			console.log('drop');
+			this.append(img, img.src = "../wicket/resource/sbtech.NG.AlphaLN/alphaimg/" + img + ".jpg", img.classList.add('fill'), img.setAttribute("draggable", true));
+			
+		});
+}
+
+for (let i = 0; i < choiceContainer.length; i++){
+		
+		const item = choiceContainer[i];
+		
+		item.addEventListener('dragstart', function dragEnter() {
+						draggedItem = item;
+						item.className += ' hold';
+						setTimeout(()=> item.className = "invisable",0);
+				});
+		item.addEventListener('dragend', function dragEnd() {
+						item.className  = "fill";
+				});
+}
+aimg.forEach(function createImage(image){
+				const img = document.createElement('img');
+				img.src = "../wicket/resource/sbtech.NG.AlphaLN/alphaimg/" + image + ".jpg";
+				img.classList.add('fill');
+				img.setAttribute("draggable", true);
+				img.id = image;
+				choiceContainer.append(img);	
+			return image;
+			});
+			
 
 function Buildalphabet(letter){
 	
@@ -51,7 +81,7 @@ function Buildalphabet(letter){
 	Buildalphabet.addEventListener("click", () =>{
 		
 		letterTitle.innerText = "" + letter + ":";
-		
+	
 	});
 
 	return Buildalphabet;
@@ -68,42 +98,18 @@ for (a = 0; a < alphabetCount; a++){
 				
 }
 
-for(const empty of empties){
-	empty.addEventListener('dragover',dragOver);
-	empty.addEventListener('dragenter', dragEnter);
-	empty.addEventListener('dragleave', dragLeave);
-	empty.addEventListener('drop', dragDrop);
-}
-
-
-function dragStart() {
-	this.className += ' hold';
-	//setTimeout(()=> this.className = "invisable",0);
-}
+/*
 
 function dragEnda() {
 		this.className  = "aletter";
-}
-function dragEndb(){
-	this.className = "bletter";
-}
-function dragOver(e) {
-	e.preventDefault();
+		
 }
 
-function dragEnter(e) {
-	e.preventDefault();
-	this.className += ' hovered';
-}
 
-function dragLeave() {
-	this.className = 'empty';
-}
+//function dragEndb(){
+//	this.className = "bletter";
+//}
 
-function dragDrop() {
-	this.className = 'empty';
-	if(this.className == "empty"){
-		this.appendChild(aContainer);
-	}
-}
+
+*/
 
