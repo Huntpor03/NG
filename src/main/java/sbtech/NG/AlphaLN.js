e0087c09cc37fe48f87862627570281fa287c4be
Huntpor03 <Huntpor03@gmail.com>
2024-06-17 08:51:55 -0700

    Added Audio to Alphabet Learning

--- src/main/java/sbtech/NG/AlphaLN.js
+++ src/main/java/sbtech/NG/AlphaLN.js
@@ -232,106 +232,158 @@ function Buildalphabet(letter){
 	Buildalphabet.addEventListener("click", () =>{
 		
 		if(Buildalphabet.id == "Aa"){
+			let Aaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/A.m4a");
+			Aaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "APE";
 		}
 		if(Buildalphabet.id == "Bb"){
+			let Baudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/B.m4a");
+			Baudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "BAT";
 		}
 		if(Buildalphabet.id == "Cc"){
+			let Caudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/C.m4a");
+			Caudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "CAT";
 		}
 		if(Buildalphabet.id == "Dd"){
+			let Daudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/D.m4a");
+			Daudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "DOG";
 		}
 		if(Buildalphabet.id == "Ee"){
+			let Eaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/E.m4a");
+			Eaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "EAT";
 		}
 		if(Buildalphabet.id == "Ff") {
+			let Faudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/F.m4a");
+			Faudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "FAT";
 		}
 		if(Buildalphabet.id == "Gg"){
+			let Gaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/G.m4a");
+			Gaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "GOD";
 		}
 		if(Buildalphabet.id == "Hh"){
+			let Haudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/H.m4a");
+			Haudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "HIT";
 		}
 		if(Buildalphabet.id == "Ii"){
+			let Iaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/I.m4a");
+			Iaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "INN";
 		}
 		if(Buildalphabet.id == "Jj"){
+			let Jaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/J.m4a");
+			Jaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "JET";
 		}
 		if(Buildalphabet.id == "Kk"){
+			let Kaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/K.m4a");
+			Kaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "KIT";
 		}
 		if(Buildalphabet.id == "Ll"){
+			let Laudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/L.m4a");
+			Laudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "LOT";
 		}
 		if(Buildalphabet.id == "Mm"){
+			let Maudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/M.m4a");
+			Maudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "MAN";
 		}
-		if(Buildalphabet.id == "Nn"){
+		if(Buildalphabet.id == "Nn"){aimg
+			let Naudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/N.m4a");
+			Naudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "NUT";
 		}
 		if(Buildalphabet.id == "Oo"){
+			let Oaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/O.m4a");
+			Oaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "OUT";
 		}
 		if(Buildalphabet.id == "Pp"){
+			let Paudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/P.m4a");
+			Paudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "PEN";
 		}
 		if(Buildalphabet.id == "Qq"){
+			let Qaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/Q.m4a");
+			Qaudio.play();
 			spell.innerText = "Find: "
 			letterTitle.innerText = "Q";
 		}
 		if(Buildalphabet.id == "Rr"){
+			let Raudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/R.m4a");
+			Raudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "RET";
 		}
 		if(Buildalphabet.id == "Ss"){
+			let Saudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/S.m4a");
+			Saudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "SUS";
 		}
 		if(Buildalphabet.id == "Tt"){
+			let Taudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/T.m4a");
+			Taudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "TIE";
 		}
 		if(Buildalphabet.id == "Uu"){
+			let Uaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/U.m4a");
+			Uaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "UP";
 		}
 		if(Buildalphabet.id == "Vv") {
+			let Vaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/V.m4a");
+			Vaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "VIB";
 		}
 		if(Buildalphabet.id == "Ww"){
+			let Waudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/W.m4a");
+			Waudio.play();
 			spell.innerText = "Sell: "
 			letterTitle.innerText = "WIG";
 		}
 		if(Buildalphabet.id == "Xx"){
+			let Xaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/X.m4a");
+			Xaudio.play();
 			spell.innerText = "Find: "
 			letterTitle.innerText = "X";
 		}
 		if(Buildalphabet.id == "Yy"){
+			let Yaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/Y.m4a");
+			Yaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "YUT"
 		}
 		if(Buildalphabet.id == "Zz"){
+			let Zaudio = new Audio("../wicket/resource/sbtech.NG.AlphaLN/audioRec/Z.m4a");
+			Zaudio.play();
 			spell.innerText = "Spell: "
 			letterTitle.innerText = "ZIG"
 		}
